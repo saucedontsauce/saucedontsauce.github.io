3b10f8d69832ccb9618f0560beaee1d06c4a6cd0
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-08-16 15:37:02 +0100

    Update CheckData.module.js

--- spouse-script/bin/CheckData.module.js
+++ spouse-script/bin/CheckData.module.js
@@ -1,13 +1,13 @@
 async function checkData(user, spouse) {
     if (!user) {
         log("%cNo local user data present", logStyle);
-        const newUserData = await fetchUserData();
+        const newUserData = await fetchUserUTIL();
         user = newUserData;
         GMSet('user_data', JSON.stringify(newUserData))
     } else {
         const jsonuser = user;
         if (Math.floor((jsonuser.timestamp * 1000) - 21600) <= Date.now() || jsonuser.error) {
-            const newUserData = await fetchUserData();
+            const newUserData = await fetchUserUTIL();
             GMSet('user_data', JSON.stringify(newUserData));
         } else {
             log("%cLocal user data present and valid", logStyle);
@@ -16,12 +16,12 @@ async function checkData(user, spouse) {
     };
     if (!spouse && user?.married?.spouse_id) {
         log("%cHas spouse but no data", logStyle);
-        const newSpouseData = await fetchSpouseData(user.married.spouse_id);
+        const newSpouseData = await fetchSpouseUTIL(user.married.spouse_id);
         spouse = newSpouseData; GMSet('local_spouse', JSON.stringify(newSpouseData));
     } else {
         const jsonspouse = spouse;
         if (Math.floor((jsonspouse.timestamp * 1000) - 21600) <= Date.now() || jsonspouse.error) {
-            const newSpouseData = await fetchSpouseData(user.married.spouse_id);
+            const newSpouseData = await fetchSpouseUTIL(user.married.spouse_id);
             spouse = newSpouseData;
             GMSet('local_spouse', JSON.stringify(newSpouseData));
         } else {
