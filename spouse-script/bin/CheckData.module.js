fa7ff78ce72ccd7b68dced41143fd10c43d74291
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-08-16 16:24:26 +0100

    bug hunting

--- spouse-script/bin/CheckData.module.js
+++ spouse-script/bin/CheckData.module.js
@@ -9,6 +9,7 @@ async function checkData(key, user, spouse) {
         GMSet('user_data', JSON.stringify(newUserData))
     } else {
         const jsonuser = user;
+        log(user)
         if (((jsonuser.timestamp * 1000) - 21600 <= Date.now()) || jsonuser.error) {
             const newUserData = await fetchUserUTIL(key);
             GMSet('user_data', JSON.stringify(newUserData));
@@ -24,6 +25,7 @@ async function checkData(key, user, spouse) {
         spouse = newSpouseData; GMSet('spouse_data', JSON.stringify(newSpouseData));
     } else {
         const jsonspouse = spouse;
+        log(spouse)
         if (((jsonspouse.timestamp * 1000) - 21600 <= Date.now()) || jsonspouse.error) {
             const newSpouseData = await fetchSpouseUTIL(key, user.married.spouse_id);
             spouse = newSpouseData;
--- spouse-script/bin/Util.module.js
+++ spouse-script/bin/Util.module.js
@@ -33,7 +33,7 @@ async function fetchUserUTIL(key) {
     try {
         const data = await fetch("https://api.torn.com/v2/user?selections=profile,display,timestamp&key=" + key);
         log(data);
-        const json = data.json();
+        const json = await data.json();
         log(json);
         if (!json.error) return json;
     } catch (err) {
@@ -45,7 +45,7 @@ async function fetchSpouseUTIL(key, id) {
     log("%cFetching spouse", logStyle);
     try {
         const data = await fetch(`https://api.torn.com/v2/user/${id}?selections=display,timestamp&key=${key}`);
-        const json = data.json();
+        const json = await data.json();
         log(json);
         if (!json.error) return json;
     } catch (err) {
