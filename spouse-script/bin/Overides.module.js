bb680a10159da1c1df76df06324b340495e3af8f
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-08-16 14:17:33 +0100

    Update Overides.module.js

--- spouse-script/bin/Overides.module.js
+++ spouse-script/bin/Overides.module.js
@@ -1,4 +1,4 @@
-log("loading Overides.module.js");
+console.log("%cloading Overides.module.js", 'background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;');
 // GENERAL
 const log = console.log;
 const logStyle = 'background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;';
