04cdd975dcc401e60bae645a82b294f07068aee8
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-08-17 17:34:40 +0100

    Update CheckData.module.js

--- spouse-script/bin/CheckData.module.js
+++ spouse-script/bin/CheckData.module.js
@@ -9,13 +9,11 @@ async function checkData(key, user, spouse) {
         GMSet('user_data', JSON.stringify(newUserData))
     } else {
         const jsonuser = user;
-        log(user)
         if ((Date.now() - (jsonuser.timestamp * 1000) > 3600000) || jsonuser.error) {
             log("%cold data", c)
             const newUserData = await fetchUserUTIL(key);
             GMSet('user_data', JSON.stringify(newUserData));
         } else {
-            log(jsonuser);
             log("%cLocal user data present and valid", logStyle);
             user = { ...jsonuser };
         };
@@ -26,14 +24,12 @@ async function checkData(key, user, spouse) {
         spouse = newSpouseData; GMSet('spouse_data', JSON.stringify(newSpouseData));
     } else {
         const jsonspouse = spouse;
-        log(spouse)
         if ((Date.now() - (jsonspouse.timestamp * 1000) > 3600000) || jsonspouse.error) {
             log("%cold data", c)
             const newSpouseData = await fetchSpouseUTIL(key, user.married.spouse_id);
             spouse = newSpouseData;
             GMSet('spouse_data', JSON.stringify(newSpouseData));
         } else {
-            log(jsonspouse);
             log("%cLocal spouse data present and valid", logStyle);
             spouse = { ...jsonspouse };
         }
