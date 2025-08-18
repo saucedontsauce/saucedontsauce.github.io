b9254a9f24b33738a62236ee2a646accaa5f945d
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2025-08-18 01:35:13 +0100

    changes
    merging

--- spouse-script/bin/Overides.module.js
+++
@@ -1,8 +0,0 @@
-// GENERAL
-const log = console.log;
-const logStyle = 'background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;';
-
-// DOM
-const $get = (selector) => document.querySelector(selector);
-const $create = (tag) => document.createElement(tag);
-
--- spouse-script/script.js
+++ spouse-script/script.js
@@ -10,9 +10,7 @@
 // @grant    GM.getValue
 // @grant    GM.setValue
 // @grant    GM.deleteValue
-// @require https://saucedontsauce.github.io/spouse-script/bin/Overides.module.js?ts=<?= Date.now() ?>
 // @require https://saucedontsauce.github.io/spouse-script/bin/Data.module.js?ts=<?= Date.now() ?>
-// @require https://saucedontsauce.github.io/spouse-script/bin/Util.module.js?ts=<?= Date.now() ?>
 // @require https://saucedontsauce.github.io/spouse-script/bin/CheckData.module.js?ts=<?= Date.now() ?>
 // @require https://saucedontsauce.github.io/spouse-script/bin/BuildingBlocks.module.js?ts=<?= Date.now() ?>
 // @require https://saucedontsauce.github.io/spouse-script/bin/SignInForm.module.js?ts=<?= Date.now() ?>
@@ -22,13 +20,20 @@
 
 (async function () {
     'use strict';
+    const log = console.log;
+    const logStyle = 'background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;';
+    const $get = (selector) => document.querySelector(selector);
+    const $create = (tag) => document.createElement(tag);
+    function setStyles(el, styles) { Object.assign(el.style, styles); };
+
+
+    function filterRow() { const fr = $create("div"); setStyles(fr, { display: "flex", padding: ".5rem 0", flexWrap: "wrap", flexDirection: "row", gap: ".5rem", rowGap: ".5rem", justifyContent: "space-evenly" }); return fr; };
+    function hr() { const hr0 = $create("hr"); hr0.id = "hr0"; hr0.style.borderTopLeftRadius = "5px !important"; hr0.style.borderBottomLeftRadius = "5px !important"; hr0.className = "page-head-delimiter border-round m-top10 m-bottom10"; return hr0; };
 
     class SpouseScript {
 
 
-        #render() {
-
-        }
+        #render(target, fnOrEl) { log("%cRendering box", logStyle); let targetDiv = $get(target); if (!targetDiv) return; log("%cTARGET FOUND", logStyle); const jubwr = $create("div"); jubwr.className = "api-form-wrapper"; const outerWrap = $create("div"); outerWrap.className = "flex border-round info-msg"; outerWrap.style.alignItems = "center"; const innerWrap = $create("div"); innerWrap.className = "delimiter border-round"; innerWrap.style.borderRadius = "5px"; let item; if (typeof fnOrEl === "function") { item = fnOrEl(); } else { item = fnOrEl; }; if (!(item instanceof HTMLElement)) { console.warn("renderBox expected a DOM node but got:", item); return; }; const hrr = hr(); innerWrap.appendChild(item); outerWrap.appendChild(innerWrap); jubwr.appendChild(outerWrap); jubwr.appendChild(hrr); targetDiv.prepend(jubwr); };
 
         #log() {
             log("%cKey: %o", logStyle, data.key);
@@ -44,8 +49,6 @@
             log("%cspouse-script - made by d00dleD", logStyle);
 
             const a = new SpouseScript();
-
-
 
            /**/ a.#log();
             const indicator = $get("h4").textContent.trim()
@@ -66,9 +69,9 @@
                     log("%c%s", logStyle, indicator);
                     if (data.key) {
                         log("%cNO KEY PRESENT", logStyle);
-                        renderBox("div.content-wrapper", createSpouseDisplay);
+                        a.#render("div.content-wrapper", createSpouseDisplay);
                     } else {
-                        renderBox("div.content-wrapper", createSignInForm);
+                        a.#render("div.content-wrapper", createSignInForm);
                     }
                     break;
                 }
